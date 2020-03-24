c7b9c42a1511f32f0d563c71fe1bb084fe047eb4
langbl28 <langbl28@gmail.com>
2020-03-24 16:15:23 -0500

    hero push

---
+++ hero-banner.js
@@ -0,0 +1,33 @@
+class DDHero extends HTMLElement {
+    constructor() {
+        super();
+    }
+    
+    connectedCallback() {
+        this.innerHTML = `
+        <style>
+        .hero-banner{
+            background-color: rgba(0,0,0,0.5);
+            background-img: url("${this.getAttribute("hero-image")}");
+        }
+        </style>
+        <section class="hero-banner">
+            <div class="container">
+            <div class="row">
+            <div class="col">
+                <p>Content</p>
+            </div>
+            </div>
+            </div>
+        </section>
+        
+        
+        </h1>
+
+        `;
+    }
+}
+
+
+    
+window.customElements.define('dd-hero', DDHero);
